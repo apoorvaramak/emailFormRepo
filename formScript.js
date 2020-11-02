9985b2cb20914ed5b6ce5a3f6327cb3b490f1a60
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-01 23:59:30 -0500

    initial commit

---
+++ formScript.js
@@ -0,0 +1,39 @@
+const first = document.getElementById("firstname").value;
+document.getElementById("firstname").addEventListener('click', e => {
+  firstNameFilled();
+});
+
+function firstNameFilled()
+{
+  if(first !== '')
+    {
+      return true;
+    }
+  else{
+    alert("please enter your first name!")
+  }
+}
+
+const last = document.getElementById("lastname").value;
+document.getElementById("firstname").addEventListener('click', e => {
+   lastNameFilled();
+});
+function lastNameFilled()
+{
+  if(first !== '')
+    {
+      return true;
+    }
+  else{
+    alert("please enter your last name!")
+  }
+}
+
+document.getElementById("submit").addEventListener('click', e => {
+   scrollWheel();
+});
+
+function scrollWheel()
+{
+
+}
