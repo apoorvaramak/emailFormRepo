5a359e5ba2bdc18da008aebd54f07eb75273da25
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-05 18:05:15 -0500

    edit email validation

--- formScript.js
+++ formScript.js
@@ -36,8 +36,17 @@ function lastNameFilled(lastName)
 }
 
 function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    // const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // return re.test(email);
+    if(email !== '')
+    {
+      return true;
+    }
+    else {
+      {
+        return false; 
+      }
+    }
   }
 
 
